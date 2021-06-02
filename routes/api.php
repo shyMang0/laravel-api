c5ca18b31c2bb7040e36aba2e1caaa078ba63c9c
shyMango <carinodrex@gmail.com>
2021-06-02 21:45:27 +0800

    init

---
+++ routes/api.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+use App\Models\Product;
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+Route::get('/products', function(){
+    return Product::all();
+});
+
+Route::post('/products', function(){
+    return 'insert';
+});
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
